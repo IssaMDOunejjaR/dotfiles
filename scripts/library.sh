87c7e9bf5012436e0323c2552c97f0a2c43a3152
IssaMDOunejjaR <otaku6issam@gmail.com>
2023-09-30 11:33:55 +0100

    add a helper func

--- scripts/library.sh
+++ scripts/library.sh
@@ -90,3 +90,40 @@ _installPackagesYay() {
   done
 
 }
+
+_installSymLink() {
+  name="$1"
+  symlink="$2";
+  linksource="$3";
+  linktarget="$4";
+
+  while true; do
+    if [ -L "${symlink}" ]; then
+      rm ${symlink}
+
+      ln -s ${linksource} ${linktarget} 
+
+      echo "Symlink ${linksource} -> ${linktarget} created."
+    else
+      if [ -d ${symlink} ]; then
+        rm -rf ${symlink}/ 
+
+        ln -s ${linksource} ${linktarget}
+
+        echo "Symlink for directory ${linksource} -> ${linktarget} created."
+      else
+        if [ -f ${symlink} ]; then
+          rm ${symlink} 
+
+          ln -s ${linksource} ${linktarget} 
+
+          echo "Symlink to file ${linksource} -> ${linktarget} created."
+        else
+          ln -s ${linksource} ${linktarget} 
+
+          echo "New symlink ${linksource} -> ${linktarget} created."
+        fi
+      fi
+    fi
+  done
+}
