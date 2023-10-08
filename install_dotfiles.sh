5fb336d8920a0f21791572a1ac51465773c8ae2e
IssaMDOunejjaR <otaku6issam@gmail.com>
2023-10-08 12:54:19 +0100

    add gtk symlinks

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -17,7 +17,7 @@ echo "by Issam Ounejjar"
 echo ""
 echo ""
 
-# Install General Synlinks
+# Install General Symlinks
 _installSymLink ~/.config/nvim ~/dotfiles/nvim ~/.config/
 _installSymLink ~/.config/dunst ~/dotfiles/dunst ~/.config/
 _installSymLink ~/.config/rofi ~/dotfiles/rofi ~/.config/
@@ -28,10 +28,16 @@ _installSymLink ~/.config/rofi ~/dotfiles/rofi ~/.config/
 _installSymLink ~/.zshrc ~/dotfiles/oh-my-zsh/.zshrc ~/.zshrc
 _installSymLink ~/wallpaper ~/dotfiles/wallpapers ~/wallpaper
 
-# Install Hyprland Synlinks
+# Install Hyprland Symlinks
 _installSymLink ~/.config/hypr ~/dotfiles/hyprland ~/.config/hypr
 _installSymLink ~/.config/waybar ~/dotfiles/waybar ~/.config/
 _installSymLink ~/.config/wlogout ~/dotfiles/wlogout ~/.config/
+
+# Install GTK symlinks
+_installSymLink ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+_installSymLink ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/
+_installSymLink ~/.Xresources ~/dotfiles/gtk/.Xresources ~/.Xresources
+_installSymLink ~/.icons ~/dotfiles/gtk/.icons/ ~/
 
 # Pywal templates initiated
 wal -i wallpapers/
