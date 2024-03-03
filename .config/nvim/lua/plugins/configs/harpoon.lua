3ee78813e88774e6eb0357ad43795373b3abb917
IssaMDOunejjaR <otaku6issam@gmail.com>
2024-03-03 15:11:25 +0100

    harpoon keybinding

--- .config/nvim/lua/plugins/configs/harpoon.lua
+++ .config/nvim/lua/plugins/configs/harpoon.lua
@@ -3,5 +3,5 @@ local mark = require("harpoon.mark")
 
 vim.keymap.set("n", "<C-q>", ui.toggle_quick_menu, {})
 vim.keymap.set("n", "<C-a>", mark.add_file, {})
-vim.keymap.set("n", "<C-j>", ui.nav_next, {})
-vim.keymap.set("n", "<C-k>", ui.nav_prev, {})
+vim.keymap.set("n", "<C-n>", ui.nav_next, {})
+vim.keymap.set("n", "<C-p>", ui.nav_prev, {})
