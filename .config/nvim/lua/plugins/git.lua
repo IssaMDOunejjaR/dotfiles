f47e879ecf9717b3bf20a8731d28458eea7d86b1
IssaMDounejjaR <otaku6issam@gmail.com>
2024-08-11 14:02:15 +0100

    add vim-fugitive and some keymaps

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,8 @@
 return {
 	{
+		"tpope/vim-fugitive",
+	},
+	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
 			signs = {
@@ -20,10 +23,15 @@ return {
 			},
 		},
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				preview_config = {
+					border = nil,
+				},
+			})
 
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
 			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", {})
 		end,
 	},
 }
