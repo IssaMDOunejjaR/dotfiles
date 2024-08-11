936fc6476d17fb1d07c473d147fd383433532b0a
IssaMDounejjaR <otaku6issam@gmail.com>
2024-08-11 14:02:48 +0100

    improve the Run command

--- .config/nvim/lua/cmds.lua
+++ .config/nvim/lua/cmds.lua
@@ -12,6 +12,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- Create a custom Neovim command to run shell commands and display output in a temporary buffer
 vim.api.nvim_create_user_command("Run", function(opts)
-	vim.cmd("new | r !" .. opts.args)
-end, { nargs = 1 })
+	-- Run the shell command and capture the output
+	local output = vim.fn.systemlist(opts.args)
+
+	-- Create a new scratch buffer
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
+
+	-- Open the scratch buffer in a new split window
+	vim.api.nvim_command("split")
+	vim.api.nvim_win_set_buf(0, buf)
+
+	-- Make the buffer read-only
+	vim.api.nvim_buf_set_option(buf, "modifiable", false)
+end, { nargs = 1, complete = "shellcmd" })
