182ff6e1de601ff314857323f9facce58bfffb97
IssaMDOunejjaR <otaku6issam@gmail.com>
2024-08-11 20:14:20 +0100

    add command to enable/disable autoformat

--- .config/nvim/lua/cmds.lua
+++ .config/nvim/lua/cmds.lua
@@ -28,3 +28,25 @@ vim.api.nvim_create_user_command("Run", function(opts)
 	-- Make the buffer read-only
 	vim.api.nvim_buf_set_option(buf, "modifiable", false)
 end, { nargs = 1, complete = "shellcmd" })
+
+vim.b.disable_autoformat = false
+vim.g.disable_autoformat = false
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat on save",
+	bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat on save",
+})
--- .config/nvim/lua/plugins/autoformat.lua
+++ .config/nvim/lua/plugins/autoformat.lua
@@ -18,7 +18,12 @@ return { -- Autoformat
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+
 			local disable_filetypes = {}
+
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -35,4 +40,7 @@ return { -- Autoformat
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
+	-- config = function()
+	-- 	vim.keymap.set("n", "<leader>lf", function() end, {})
+	-- end,
 }
