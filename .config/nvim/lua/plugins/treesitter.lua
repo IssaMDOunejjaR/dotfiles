246eee954a8a4efdecbb8610a0f7e33e099db065
IssaMDounejjaR <otaku6issam@gmail.com>
2024-11-16 12:03:57 +0100

    remove autotag from treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -27,9 +27,6 @@ return { -- Highlight, edit, and navigate code
 			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 			additional_vim_regex_highlighting = { "ruby" },
 		},
-		autotag = {
-			enable = true,
-		},
 		indent = { enable = true, disable = { "ruby" } },
 	},
 	config = function(_, opts)
