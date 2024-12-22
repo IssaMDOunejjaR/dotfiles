d4388a07484b4912f17de50706cb3874d0137d0e
IssaMDounejjaR <otaku6issam@gmail.com>
2024-12-22 13:32:37 +0100

    add mapping for image_preview plugin

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,6 +6,13 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		{
+			"adelarsq/image_preview.nvim",
+			-- event = "VeryLazy",
+			config = function()
+				require("image_preview").setup()
+			end,
+		},
 	},
 	config = function()
 		require("neo-tree").setup({
@@ -25,6 +32,20 @@ return {
 				},
 				follow_current_file = {
 					enabled = true,
+				},
+				window = {
+					mappings = {
+						["<leader>p"] = "image_kitty", -- " or another map
+					},
+				},
+				commands = {
+					image_kitty = function(state)
+						local node = state.tree:get_node()
+
+						if node.type == "file" then
+							require("image_preview").PreviewImage(node.path)
+						end
+					end,
 				},
 				use_libuv_file_watcher = true,
 			},
