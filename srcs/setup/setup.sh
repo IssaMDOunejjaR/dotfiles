8c40f717c05197ad5f7afa795c6cfbeb407b145c
IssaMDounejjaR <otaku6issam@gmail.com>
2025-01-07 19:17:07 +0100

    add playbook for setup ubuntu

--- srcs/setup/setup.sh
+++ srcs/setup/setup.sh
@@ -5,4 +5,7 @@ sudo pacman -Sy --noconfirm reflector
 sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Sy --noconfirm python3 ansible
 
-cd ~/dotfiles/srcs/setup/arch && ansible-playbook ./playbook.yml && cd -
+cd ~/dotfiles/srcs/setup && ansible-playbook ./arch.yml && cd -
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3 ansible
