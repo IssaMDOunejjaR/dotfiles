fefcd5481cf21f0b31dafb69e3c3630bd854cb6b
IssaMDounejjaR <otaku6issam@gmail.com>
2025-01-10 19:45:46 +0100

    remove the sudo check

--- srcs/setup/setup.sh
+++ srcs/setup/setup.sh
@@ -2,27 +2,20 @@
 
 type=$1
 
-# Ensure the script is run with sudo
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Please run this script with sudo:"
-  echo "  sudo $0 <arch|ubuntu>"
-  exit 1
-fi
-
 cd ~/dotfiles/srcs/setup
 
 case $type in
   "arch")
-    pacman -Syyu --noconfirm
-    pacman -Sy --noconfirm reflector
-    reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-    pacman -Sy --noconfirm python3 ansible
+    sudo pacman -Syyu --noconfirm
+    sudo pacman -Sy --noconfirm reflector
+    sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+    sudo pacman -Sy --noconfirm python3 ansible
     
     ansible-playbook ./arch.yml
     ;;
   "ubuntu")
-    apt update
-    apt install -y python3 ansible
+    sudo apt update
+    sudo apt install -y python3 ansible
     ansible-galaxy collection install community.general --upgrade
     
     ansible-playbook ./ubuntu.yml
@@ -36,6 +29,6 @@ case $type in
 esac
 
 if [ $? -eq 0 ]; then
-  ansible-playbook ./config.yml
+  sudo ansible-playbook ./config.yml
   cd -
 fi
