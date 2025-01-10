51e853c6dc835c0c179bd42d58f0bdcdd4c79702
IssaMDounejjaR <otaku6issam@gmail.com>
2025-01-10 14:42:00 +0100

    refactor setup script

--- srcs/setup/setup.sh
+++ srcs/setup/setup.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
-sudo pacman -Syyu --noconfirm
-sudo pacman -Sy --noconfirm reflector
-sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Sy --noconfirm python3 ansible
+type=$1
 
-cd ~/dotfiles/srcs/setup && ansible-playbook ./arch.yml && cd -
+cd ~/dotfiles/srcs/setup
 
-sudo apt update
-sudo apt install -y python3 ansible
-ansible-galaxy collection install community.general --upgrade
+case type in
+  "arch")
+    sudo pacman -Syyu --noconfirm
+    sudo pacman -Sy --noconfirm reflector
+    sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+    sudo pacman -Sy --noconfirm python3 ansible
+    
+    ansible-playbook ./arch.yml
+    ;;
+  "ubuntu")
+    sudo apt update
+    sudo apt install -y python3 ansible
+    ansible-galaxy collection install community.general --upgrade
+    
+    ansible-playbook ./ubuntu.yml
+    ;;
+
+  *)
+    echo "Invalid Argument!"
+    echo "Available Arguments: arch, ubuntu"
+    ;;
+esac
+
+if $? == 0; then
+  ansible-playbook ./config.yml
+  cd -
+fi
