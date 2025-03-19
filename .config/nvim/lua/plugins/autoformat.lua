d4d93eb15634a5a8cc057e63edf883cac3aeb708
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-03-19 22:36:57 +0000

    update plugins and add biome as a formatter for js and ts files

--- .config/nvim/lua/plugins/autoformat.lua
+++ .config/nvim/lua/plugins/autoformat.lua
@@ -44,8 +44,8 @@ return { -- Autoformat
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 
-			javascript = { "prettier" },
-			typescript = { "prettier" },
+			javascript = { "biome" },
+			typescript = { "biome" },
 		},
 	},
 }
