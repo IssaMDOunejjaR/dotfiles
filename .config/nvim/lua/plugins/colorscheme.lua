4a67c5b60de7aeae8d2f5ffcd487ef0091366a16
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-04-06 16:29:12 +0100

    change options for colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,15 @@ return {
 	"sainnhe/edge",
 	priority = 1000, -- Ensure it loads first
 	config = function()
-		vim.g.edge_style = "default"
+		vim.g.edge_style = "neon"
+		-- vim.g.edge_dim_foreground = 1
 		vim.g.edge_better_performance = 1
-		vim.g.edge_float_style = "dim"
+		vim.g.edge_float_style = "bright"
 		vim.g.edge_diagnostic_text_highlight = 1
 		vim.g.edge_diagnostic_line_highlightd = 1
 		vim.g.edge_diagnostic_virtual_text = "colored"
+		vim.g.edge_inlay_hints_background = "dimmed"
+		-- vim.g.edge_enable_italic = true
 
 		vim.cmd.colorscheme("edge")
 	end,
