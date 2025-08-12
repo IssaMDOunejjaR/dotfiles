ca1226bbe34ed9bfd04efad8e504f0750d9ea1d4
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-08-12 16:12:12 +0100

    refactor arch setup to use hyprland instead of i3

--- srcs/setup/setup.sh
+++ srcs/setup/setup.sh
@@ -2,33 +2,33 @@
 
 type=$1
 
-cd ~/dotfiles/srcs/setup
+cd ~/dotfiles/srcs/setup || exit 1
 
 case $type in
-  "arch")
-    sudo pacman -Syyu --noconfirm
-    sudo pacman -Sy --noconfirm reflector
-    sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-    sudo pacman -Sy --noconfirm python3 ansible
-    
-    ansible-playbook ./arch.yml --ask-become-pass
-    ;;
-  "ubuntu")
-    sudo apt update
-    sudo apt install -y python3 ansible
-    ansible-galaxy collection install community.general --upgrade
-    
-    ansible-playbook ./ubuntu.yml --ask-become-pass
-    ;;
+"arch")
+  sudo pacman -Syyu --noconfirm
+  sudo pacman -Sy --noconfirm reflector
+  sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+  sudo pacman -Sy --noconfirm python3 ansible
 
-  *)
-    echo "Invalid Argument!"
-    echo "Available Arguments: arch, ubuntu"
-    exit
-    ;;
+  ansible-playbook ./arch.yml --ask-become-pass
+  ;;
+"ubuntu")
+  sudo apt update
+  sudo apt install -y python3 ansible
+  ansible-galaxy collection install community.general --upgrade
+
+  ansible-playbook ./ubuntu.yml --ask-become-pass
+  ;;
+
+*)
+  echo "Invalid Argument!"
+  echo "Available Arguments: arch, ubuntu"
+  exit
+  ;;
 esac
 
 if [ $? -eq 0 ]; then
   ansible-playbook ./config.yml --ask-become-pass
-  cd -
+  cd - || exit 1
 fi
