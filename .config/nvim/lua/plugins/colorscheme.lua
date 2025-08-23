a680d69ac21c8c74b4ad3412aff4470ee8ed53b5
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-08-23 17:14:35 +0100

    update plugins and change coloscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,18 @@
 return {
-	"navarasu/onedark.nvim",
+	-- "navarasu/onedark.nvim",
+	-- "EdenEast/nightfox.nvim",
+	"bluz71/vim-moonfly-colors",
+	-- "scottmckendry/cyberdream.nvim",
+	lazy = false,
 	priority = 1000, -- Ensure it loads first
 	config = function()
-		require("onedark").setup({
-			style = "darker",
-		})
+		-- require("onedark").setup({
+		-- 	style = "darker",
+		-- })
 
-		vim.cmd.colorscheme("onedark")
+		-- vim.cmd.colorscheme("onedark")
+		-- vim.cmd.colorscheme("carbonfox")
+		vim.cmd.colorscheme("moonfly")
+		-- vim.cmd.colorscheme("cyberdream")
 	end,
 }
