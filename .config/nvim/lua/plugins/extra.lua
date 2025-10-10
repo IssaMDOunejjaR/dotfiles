fc1f9cbda6f46ae26fad419afd4d0465300f35ff
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-10-10 20:23:02 +0100

    update neovim config to use AstroNvim

--- .config/nvim/lua/plugins/extra.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"tpope/vim-sleuth",
-
-	{
-		"christoomey/vim-tmux-navigator",
-		keys = { "<C-h>", "<C-l>", "<C-j>", "<C-k>" },
-	},
-}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,52 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-		"nvim-treesitter/nvim-treesitter-context",
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	config = function()
-		-- Ensure dependencies are loaded safely
-		local has_autotag, autotag = pcall(require, "nvim-ts-autotag")
-
-		if has_autotag then
-			autotag.setup({
-				opts = {
-					enable_close = true,
-					enable_rename = true,
-					enable_close_on_slash = true,
-				},
-			})
-		else
-			vim.notify("nvim-ts-autotag not found, skipping autotag setup.", vim.log.levels.WARN)
-		end
-
-		-- Load Treesitter configuration
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true, -- Automatically install missing parsers
-			highlight = {
-				enable = true, -- Enable syntax highlighting
-				additional_vim_regex_highlighting = { "ruby" }, -- Use Vim regex for Ruby
-			},
-			indent = {
-				enable = true,
-				disable = { "ruby" }, -- Disable indenting for Ruby
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
 }
