88ee43527637d7ad8c62d7a9517a5a69e824ea95
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-10-11 20:29:09 +0100

    custom oil config

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,36 @@
+return {
+  "stevearc/oil.nvim",
+
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = false,
+    lsp_file_methods = {
+      autosave_changes = true,
+    },
+    constrain_cursor = "name",
+    watch_for_changes = true,
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      max_width = 150,
+      max_height = 150,
+      padding = 3,
+      border = "single",
+    },
+  },
+
+  keys = {
+    {
+      "<leader>e",
+      function() require("oil").toggle_float(vim.fn.getcwd()) end,
+      desc = "Toggle Oil [E]xplorer root directory",
+    },
+    {
+      "<leader>c",
+      function() require("oil").toggle_float() end,
+      desc = "Toggle Oil [C]urrent directory",
+    },
+  },
+}
