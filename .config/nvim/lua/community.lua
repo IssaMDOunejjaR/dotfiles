887bc59fe7d4b6e56d0d3f0fc99c8ce205ac74fb
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-10-12 11:02:12 +0100

    fix nvim-lint typo

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -15,7 +15,7 @@ return {
   { import = "astrocommunity.fuzzy-finder.fzf-lua" },
   { import = "astrocommunity.diagnostics.error-lens-nvim" },
   { import = "astrocommunity.editing-support.bigfile-nvim" },
-  { import = "astrocommunity.lsp.nvim-lit" },
+  { import = "astrocommunity.lsp.nvim-lint" },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.lsp.garbage-day-nvim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
