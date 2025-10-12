a6e269af1f9f422cda458afd7855dae873f2a86a
IssaMDOunejjaR <otaku6issam@gmail.com>
2025-10-12 18:25:29 +0100

    add toggle auto format

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,6 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    format_on_save = {
-      timeout_ms = 3000,
-      lsp_fallback = true,
-    },
     formatters_by_ft = {
       javascript = { "prettier" },
       typescript = { "prettier" },
@@ -17,5 +13,10 @@ return {
       markdown = { "prettier" },
       lua = { "stylua" },
     },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
   },
 }
--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,5 +1,15 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.api.nvim_create_user_command("ToggleFormat", function()
+  if vim.g.disable_autoformat then
+    vim.g.disable_autoformat = false
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Toggle Auto Format",
+})
