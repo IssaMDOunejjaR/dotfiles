7093580232f752548ee141a7f8cd874e0e0fbf78
Issam Ounejjar <iounejjar@sqli.com>
2026-02-08 13:29:49 +0100

    change eslint_d to eslint

--- .config/nvim/lua/utils/lsp-auto-install.lua
+++ .config/nvim/lua/utils/lsp-auto-install.lua
@@ -1,81 +1,97 @@
-local mason_registry = require "mason-registry"
+local mason_registry = require("mason-registry")
 
 local function ensure_installed(tool_name)
-  local ok, pkg = pcall(mason_registry.get_package, tool_name)
+	local ok, pkg = pcall(mason_registry.get_package, tool_name)
 
-  if not ok then
-    return
-  end
+	if not ok then
+		return
+	end
 
-  if ok and not pkg:is_installed() then
-    pkg:install()
-  end
+	if ok and not pkg:is_installed() then
+		pkg:install()
+	end
 end
 
 local html_tools = { "html-lsp", "emmet-language-server", "prettierd" }
 
 if vim.fs.find("angular.json", { upward = true })[1] ~= nil then
-  table.insert(html_tools, "angular-language-server")
-  table.insert(html_tools, "tsgo")
+	table.insert(html_tools, "angular-language-server")
+	table.insert(html_tools, "tsgo")
 end
 
 local filetype_tools = {
-  lua = { "lua-language-server", "stylua", "luacheck" },
+	lua = { "lua-language-server", "stylua", "luacheck" },
 
-  asm = { "asm-lsp", "asmfmt" },
-  nasm = { "asm-lsp", "asmfmt" },
+	asm = { "asm-lsp", "asmfmt" },
+	nasm = { "asm-lsp", "asmfmt" },
 
-  c = { "clangd", "clang-format", "cpplint" },
-  cpp = { "clangd", "clang-format", "cpplint" },
+	c = { "clangd", "clang-format", "cpplint" },
+	cpp = { "clangd", "clang-format", "cpplint" },
 
-  javascript = { "tsgo", "prettierd", "js-debug-adapter" },
-  javascriptreact = { "tsgo", "rustywind", "prettierd", "js-debug-adapter", "html-lsp", "emmet-language-server" },
-  typescript = { "tsgo", "prettierd", "js-debug-adapter" },
-  typescriptreact = { "tsgo", "rustywind", "prettierd", "js-debug-adapter", "html-lsp", "emmet-language-server" },
+	javascript = { "tsgo", "prettierd", "eslint", "js-debug-adapter" },
+	javascriptreact = {
+		"tsgo",
+		"rustywind",
+		"prettierd",
+		"eslint",
+		"js-debug-adapter",
+		"html-lsp",
+		"emmet-language-server",
+	},
+	typescript = { "tsgo", "prettierd", "eslint", "js-debug-adapter" },
+	typescriptreact = {
+		"tsgo",
+		"rustywind",
+		"prettierd",
+		"eslint",
+		"js-debug-adapter",
+		"html-lsp",
+		"emmet-language-server",
+	},
 
-  html = html_tools,
-  htmlangular = {
-    "html-lsp",
-    "emmet-language-server",
-    "angular-language-server",
-    "prettierd",
-    "tsgo",
-    "js-debug-adapter",
-  },
+	html = html_tools,
+	htmlangular = {
+		"html-lsp",
+		"emmet-language-server",
+		"angular-language-server",
+		"prettierd",
+		"tsgo",
+		"js-debug-adapter",
+	},
 
-  css = { "css-lsp", "prettierd" },
-  scss = { "css-lsp", "prettierd" },
+	css = { "css-lsp", "prettierd" },
+	scss = { "css-lsp", "prettierd" },
 
-  json = { "json-lsp", "prettierd", "jsonlint" },
+	json = { "json-lsp", "prettierd", "jsonlint" },
 
-  python = { "basedpyright", "ruff" },
+	python = { "basedpyright", "ruff" },
 
-  bash = { "bash-language-server", "shfmt", "shellcheck" },
-  sh = { "bash-language-server", "shfmt", "shellcheck" },
-  zsh = { "bash-language-server", "shfmt", "shellcheck" },
+	bash = { "bash-language-server", "shfmt", "shellcheck" },
+	sh = { "bash-language-server", "shfmt", "shellcheck" },
+	zsh = { "bash-language-server", "shfmt", "shellcheck" },
 
-  go = { "gopls", "goimports", "gomodifytags" },
-  templ = { "gopls", "goimports", "gomodifytags", "templ" },
+	go = { "gopls", "goimports", "gomodifytags" },
+	templ = { "gopls", "goimports", "gomodifytags", "templ" },
 
-  dockerfile = { "docker-language-server", "dockerfile-language-server", "hadolint" },
-  ["yaml.docker-compose"] = { "docker-language-server", "docker-compose-language-service" },
+	dockerfile = { "docker-language-server", "dockerfile-language-server", "hadolint" },
+	["yaml.docker-compose"] = { "docker-language-server", "docker-compose-language-service" },
 
-  java = { "jdtls", "lemminx", "google-java-format", "checkstyle " },
+	java = { "jdtls", "lemminx", "google-java-format", "checkstyle " },
 
-  sql = { "sqls", "sqlfmt", "sqlfluff " },
+	sql = { "sqls", "sqlfmt", "sqlfluff " },
 
-  yaml = { "yaml-language-server", "yamllint", "yamlfmt" },
-  ["yaml.ansible"] = { "ansible-language-server", "ansible-lint" },
+	yaml = { "yaml-language-server", "yamllint", "yamlfmt" },
+	["yaml.ansible"] = { "ansible-language-server", "ansible-lint" },
 }
 
 vim.api.nvim_create_autocmd("FileType", {
-  callback = function(args)
-    local tools = filetype_tools[args.match]
+	callback = function(args)
+		local tools = filetype_tools[args.match]
 
-    if tools then
-      for _, tool in ipairs(tools) do
-        ensure_installed(tool)
-      end
-    end
-  end,
+		if tools then
+			for _, tool in ipairs(tools) do
+				ensure_installed(tool)
+			end
+		end
+	end,
 })
