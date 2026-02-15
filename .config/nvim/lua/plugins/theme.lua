57550f5ecb3f745d71fa91568018d2ffcb9760de
IssaMDOunejjaR <otaku6issam@gmail.com>
2026-02-15 15:49:35 +0000

    format

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,25 +1,25 @@
 return {
-  {
-    "cpea2506/one_monokai.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("one_monokai").setup {
-        transparent = true,
-      }
+	{
+		"cpea2506/one_monokai.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("one_monokai").setup({
+				transparent = true,
+			})
 
-      vim.cmd.colorscheme "one_monokai"
+			vim.cmd.colorscheme("one_monokai")
 
-      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#444444", bg = "#000000" })
-      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#444444", bg = "NONE" })
+			vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
 
-      vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#444444", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#444444", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "BlinkCmpScrollBarThumb", { bg = "#444444" })
-      vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#333333" })
+			vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#444444", bg = "NONE" })
+			vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#444444", bg = "NONE" })
+			vim.api.nvim_set_hl(0, "BlinkCmpScrollBarThumb", { bg = "#444444" })
+			vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#333333" })
 
-      vim.api.nvim_set_hl(0, "CustomSnacksIndent", { fg = "#222222" })
-      vim.api.nvim_set_hl(0, "CustomSnacksIndentScope", { fg = "#ff4d4d" })
-    end,
-  },
+			vim.api.nvim_set_hl(0, "CustomSnacksIndent", { fg = "#222222" })
+			vim.api.nvim_set_hl(0, "CustomSnacksIndentScope", { fg = "#ff4d4d" })
+		end,
+	},
 }
