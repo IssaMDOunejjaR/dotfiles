da3bbe8ba2f467ba01bc481a33622241e6207323
IssaMDOunejjaR <otaku6issam@gmail.com>
2026-02-15 15:49:08 +0000

    cleanup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,45 +2,55 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} }, -- LSP/DAP/Linter installer & manager
-    "mason-org/mason-lspconfig.nvim",
-		-- "creativenull/efmls-configs-nvim", -- Preconfigured EFM Language Server setups
-		-- "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
+		"mason-org/mason-lspconfig.nvim",
 	},
 	config = function()
 		-- require("utils.diagnostics").setup()
 		-- require("servers")
 
-    require("mason").setup {
-      ui = {
-        border = "single",
-      },
-    }
+		require("mason").setup({
+			ui = {
+				border = "single",
+			},
+		})
 
-    local mason = require "mason-lspconfig"
-    local servers = mason.get_installed_servers()
-    -- local Lsp = require "utils.lsp"
+		require("mason-lspconfig").setup({ ensure_installed = {}, automatic_enable = true })
 
-    require("mason-lspconfig").setup {
-      ensure_installed = {},
-      automatic_enable = true,
-    }
+		vim.lsp.config("bashls", {
+			filetypes = { "bash", "sh", "zsh" },
+		})
 
-    -- for _, server in ipairs(servers) do
-    --   vim.lsp.config(server, {
-    --     on_attach = Lsp.on_attach,
-    --   })
-    -- end
+		vim.lsp.config("vtsls", {
+			on_attach = function()
+				vim.keymap.set("n", "gS", function()
+					require("vtsls").commands.goto_source_definition(0)
+				end, { desc = "Go to Source Definition" })
+			end,
+		})
 
-    vim.lsp.config("bashls", {
-      filetypes = { "bash", "sh", "zsh" },
-    })
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					workspace = {
+						-- size in KB; raise above ~660
+						maxPreload = 20000,
+						preloadFileSize = 20000,
+						ignoreDir = {
+							vim.fn.stdpath("data") .. "/mason",
+						},
+					},
+					misc = {
+						-- reduce noise like “Too large file … skipped”
+						logLevel = "warning", -- or "error"
+					},
+				},
+			},
+		})
 
-    vim.lsp.config("vtsls", {
-      on_attach = function()
-        vim.keymap.set("n", "gS", function()
-          require("vtsls").commands.goto_source_definition(0)
-        end, { desc = "Go to Source Definition" })
-      end,
-    })
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+			border = border,
+			max_width = 50,
+			max_height = 25,
+		})
 	end,
 }
